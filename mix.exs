f2b19b1eba6a1b727579302661e673ce9fec7b1a
David JULIEN <david.julien@wttj.co>
2025-03-11 10:19:49 +0100

    chore: relax certifi version

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule Eximap.Mixfile do
       {:credo, "~> 0.8.8", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.19", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.7.4", only: [:dev, :test], runtime: false},
-      {:certifi, "~> 2.12.0"}
+      {:certifi, "~> 2.12"}
     ]
   end
 
